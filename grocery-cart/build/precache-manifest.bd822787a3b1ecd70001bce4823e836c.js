5ee9bd45e1a0fd0f417eebf2bde1504cff6ba9c0
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2019-10-05 00:47:14 +0530

    adds grocery-cart

---
+++ grocery-cart/build/precache-manifest.bd822787a3b1ecd70001bce4823e836c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b45084d4a24067e39fe0e759181ae8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6ed034454fae832dca6",
+    "url": "/static/css/2.ab5dcbdc.chunk.css"
+  },
+  {
+    "revision": "3785d2f1d46104e291b8",
+    "url": "/static/css/main.d754b923.chunk.css"
+  },
+  {
+    "revision": "c6ed034454fae832dca6",
+    "url": "/static/js/2.1560a6ef.chunk.js"
+  },
+  {
+    "revision": "3785d2f1d46104e291b8",
+    "url": "/static/js/main.28b27f67.chunk.js"
+  },
+  {
+    "revision": "94265a2e0bda0aa413e0",
+    "url": "/static/js/runtime-main.e6ce60ff.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
